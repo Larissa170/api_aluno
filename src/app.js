a906bb65e2a39aaca05611365f91f0f6a416b3ee
Larissa170 <larissa.m.rodrigues1702@gmail.com>
2023-07-13 09:34:40 -0300

    testes api com tds os verbos http
    Signed-off-by: Larissa170 <larissa.m.rodrigues1702@gmail.com>

--- src/app.js
+++ src/app.js
@@ -3,14 +3,15 @@ const { MongoClient } = require("mongodb");
 const AlunoRepository = require("./repository");
 
 const app = express();
-const dsn = "mongodb://root:root@localhost?retryWrites=true&writeConcern=majority";
+app.use(express.json());
+const dsn =
+  "mongodb://root:root@localhost?retryWrites=true&writeConcern=majority";
 const client = new MongoClient(dsn);
-
 
 app.get("/alunos", async (request, response) => {
   await client.connect();
   const collection = client.db("ap_db").collection("alunos");
-  const repository = new AlunoRepository(collection); 
+  const repository = new AlunoRepository(collection);
 
   const alunos = await repository.findAll();
   await client.close();
@@ -18,4 +19,75 @@ app.get("/alunos", async (request, response) => {
   response.json(alunos);
 });
 
+app.post("/alunos", async (request, response) => {
+  await client.connect();
+  const collection = client.db("ap_db").collection("alunos");
+  const repository = new AlunoRepository(collection);
+
+  try {
+    const aluno = await repository.create(request.body);
+    response.status(201).json(aluno);
+  } catch (e) {
+    response.status(500).json({ error: e.message });
+  }
+
+  await client.close();
+});
+app.get("/alunos/:id", async (request, response) => {
+  await client.connect();
+  const collection = client.db("ap_db").collection("alunos");
+  const repository = new AlunoRepository(collection);
+  try {
+    const aluno = await repository.findById(request.params.id);
+    if (aluno === null) {
+      response.status(404).json({
+        status: 404,
+        error: "Aluno não encontrado",
+      });
+    } else {
+      response.json(aluno);
+    }
+  } catch (e) {
+    console.log(e);
+    response.status(500).json({ error: e.message });
+  }
+  await client.close();
+});
+
+app.put("/alunos/:id", async (request, response) => {
+  await client.connect();
+  const collection = client.db("ap_db").collection("alunos");
+  const repository = new AlunoRepository(collection);
+  const aluno = await repository.findById(request.params.id);
+  if (aluno === null) {
+    response.status(404).json({
+      status: 404,
+      error: "Aluno não encontrado",
+    });
+  } else {
+    const newAluno = { ...aluno, ...request.body };
+    await repository.update(newAluno);
+    response.json(newAluno);
+  }
+
+  await client.close();
+});
+app.delete("/alunos/:id", async (request, response) => {
+  await client.connect();
+  const collection = client.db("ap_db").collection("alunos");
+  const repository = new AlunoRepository(collection);
+  const aluno = await repository.findById(request.params.id);
+
+  if (null !== aluno) {
+    await repository.delete(aluno);
+    response.status(204).send();
+  } else {
+    response.status(404).json({
+      status: 404,
+      error: "Aluno não encontrado",
+    });
+  }
+
+  await client.close();
+});
 module.exports = app;
--- src/app.spec.js
+++ src/app.spec.js
@@ -23,26 +23,144 @@ describe("API de CRUD Alunos", () => {
   beforeEach(async () => {
     await repository.deleteAll();
   });
+  afterEach(async () => {
+    //await client.close();
+  });
 
   test("GET /alunos", async () => {
     await repository.create({
       nome: "Laura",
-      idade: 15
+      idade: 15,
     });
 
-    const response = await request.get("/alunos").expect("Content-type", /application\/json/);
+    const response = await request
+      .get("/alunos")
+      .expect("Content-type", /application\/json/);
+
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBe(1);
+
     expect(response.body[0]).toStrictEqual(
       expect.objectContaining({
         nome: "Laura",
-        idade: 15
-      })    
+        idade: 15,
+      })
     );
   });
-  test.todo("POST /alunos");
 
-  test.todo("GET /alunos/;id");
-  test.todo("PUT /alunos/:id");
-  test.todo("DELETE /alunos/:id");
+  test("POST /alunos", async () => {
+    const aluno = {
+      nome: "Laura",
+      idade: 20,
+    };
+
+    const response = await request.post("/alunos").send(aluno);
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toStrictEqual(expect.objectContaining(aluno));
+  });
+
+  describe("GET /alunos/:id", () => {
+    test("deve retornar 200 para um aluno existente", async () => {
+      const aluno = await repository.create({
+        nome: "Lucas",
+        idade: 11,
+      });
+
+      const response = await request
+        .get(`/alunos/${aluno._id}`)
+        .expect("Content-type", /application\/json/);
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toStrictEqual(
+        expect.objectContaining({
+          nome: "Lucas",
+          idade: 11,
+        })
+      );
+    });
+
+    test("deve retornar 404 para aluno nao existente", async () => {
+      const response = await request
+        .get(`/alunos/64aefbdb1b0aba3d5e1fec4c`)
+        .expect("Content-type", /application\/json/);
+      expect(response.statusCode).toBe(404);
+      expect(response.body).toStrictEqual({
+        status: 404,
+        error: "Aluno não encontrado",
+      });
+    });
+  });
+  describe("PUT /alunos/:id", () => {
+    test("Deve retornar 200 para um aluno existente", async () => {
+      const aluno = await repository.create({
+        nome: "Luna",
+        idade: 12,
+      });
+
+      const response = await request
+        .put(`/alunos/${aluno._id}`)
+        .send({
+          nome: "Luna",
+          idade: 20,
+        })
+        .expect("Content-type", /application\/json/);
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toStrictEqual(
+        expect.objectContaining({
+          nome: "Luna",
+          idade: 20,
+        })
+      );
+      const newAluno = await repository.findById(aluno._id);
+      expect(newAluno).toStrictEqual(
+        expect.objectContaining({
+          nome: "Luna",
+          idade: 20,
+        })
+      );
+    });
+
+    test("Deve retornar 404 para um aluno inexistente", async () => {
+      const response = await request
+        .put("/alunos/64aefbdb1b0aba3d5e1fec4c")
+        .send({
+          nome: "Laura",
+          idade: 15,
+        })
+        .expect("Content-type", /application\/json/);
+
+      expect(response.statusCode).toBe(404);
+      expect(response.body).toStrictEqual({
+        status: 404,
+        error: "Aluno não encontrado",
+      });
+    });
+  });
+  describe("DELETE /alunos/:id", () => {
+    test("Deve retornar 204 para aluno existente", async () => {
+      const aluno = await repository.create({
+        nome: "Luna",
+        idade: 12,
+      });
+
+      const response = await request.delete(`/alunos/${aluno._id}`);
+
+      expect(response.statusCode).toBe(204);
+      expect(response.body).toStrictEqual({});
+      const newAluno = await repository.findById(aluno._id);
+      expect(newAluno).toBe(null);
+    });
+    test("Deve retornar 404 para aluno inexistente", async () => {
+      const response = await request
+        .delete("/alunos/64aefbdb1b0aba3d5e1fec4c")
+        .expect("Content-type", /application\/json/);
+
+      expect(response.statusCode).toBe(404);
+      expect(response.body).toStrictEqual({
+        status: 404,
+        error: "Aluno não encontrado",
+      });
+    });
+  });
 });
